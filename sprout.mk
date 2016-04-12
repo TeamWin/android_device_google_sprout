01b1966909fd0f6b4019a2b8288ce07c1a20fe6d
Harsh Shandilya <MSF-Jarvis@users.noreply.github.com>
2016-04-12 13:51:48 -0700

    Fix TWRP not booting on most sprout devices
    This is a combination of 4 commits.
    
     The first commit's message is:
    sprout : CM recovery kernel
    
     This is the 2nd commit message:
    
    sprout : kernel from booting, ported TWRP 3.0.2-0
    
    If this doesn't boot, I'll die. Period
     This is the 3rd commit message:
    
    sprout : Overhaul device tree
    
    Updated all *.rc's with the latest copy-paste TWRP booting on device
    
    Signed-off-by: MSF Jarvis <msfjarvis@gmail.com>
    
     This is the 4th commit message:
    
    Use precompiled ramdisk contents
    
    Signed-off-by: MSF-Jarvis <msfjarvis@gmail.com>
    
    Without these, the device simply fails to boot into recovery on newer firmwares.
    
    Change-Id: Id386ce078acba152a5c306733237d60aab228880

--- sprout.mk
+++ sprout.mk
@@ -21,7 +21,4 @@
 #$(call inherit-product, device/google/sprout-common/sprout.mk)
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/root/init.sprout.rc:root/init.sprout.rc \
-    $(LOCAL_PATH)/rootdir/root/fstab.sprout:root/fstab.sprout \
-    $(LOCAL_PATH)/rootdir/root/kernel:kernel \
-    $(LOCAL_PATH)/rootdir/root/twrp.fstab:root/twrp.fstab
+    $(LOCAL_PATH)/rootdir/root/kernel:kernel
