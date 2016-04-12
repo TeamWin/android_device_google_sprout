0d1df6fdcd5bc0322cc86ff4dd4ecb668047ccfc
Harsh Shandilya <MSF-Jarvis@users.noreply.github.com>
2016-04-12 12:13:52 +0530

    Update for unified recovery only build

--- cm.mk
+++ cm.mk
@@ -1,15 +1,15 @@
-$(call inherit-product, device/google/sprout4/sprout4.mk)
+$(call inherit-product-if-exists, device/google/sprout/sprout.mk)
 
 # Common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product-if-exists, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
 
-PRODUCT_NAME := cm_sprout4
-PRODUCT_DEVICE :=sprout4
+PRODUCT_NAME := cm_sprout
+PRODUCT_DEVICE :=sprout
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
 PRODUCT_MODEL := A1
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="sprout4"
+    PRODUCT_DEVICE="sprout"
